4e39fa03fb8806f43ebd16c187c445c54399f98a
Rodrigo Villar <rodrigojvillarg@gmail.com>
2026-01-18 16:36:49 -0500

    refactor: stowrc

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -35,6 +35,7 @@
     #   echo "Hello, ${config.home.username}!"
     # '')
     pkgs.starship
+    pkgs.neovim
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
