3d657f8009b882cb9ea381165cce4b04755e8781
rodrigo <77309055+RodrigoVillar@users.noreply.github.com>
2026-03-28 10:41:06 -0400

    chore: remove stow dep (#6)

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -41,19 +41,12 @@
     pkgs.ripgrep
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    ".config/nvim".source = ./../nvim;
+    ".config/ghostty".source = ./../ghostty;
+    ".config/lazygit".source = ./../lazygit;
+    ".config/nix".source = ./../nix;
+    ".config/starship.toml".source = ./../starship.toml;
   };
 
   # Home Manager can also manage your environment variables through
